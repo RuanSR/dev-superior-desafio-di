03277c54849632523cb1b4e3d66ab3534a352b69
RuanSR <ruansilvarosa@gmail.com>
2023-06-11 12:00:33 -0300

    project started

---
+++ src/main/java/com/ruansr/desafiodi/DesafioDiApplication.java
@@ -0,0 +1,13 @@
+package com.ruansr.desafiodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruansr/desafiodi/DesafioDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruansr.desafiodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
